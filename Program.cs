635854fd8b82ab2b1f08000c1c3f4b49e961dfd4
Mikola <mikolagil@gmail.com>
2019-07-04 21:40:28 +0300

    Add time greeting

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+            int currentHour = currentTime.Hours;
+
+            if (currentHour > 9 && currentHour <= 12)
+            {
+                Console.WriteLine("Good Morning, Guys");
+            }
+            else if (currentHour > 12 && currentHour <= 15)
+            {
+                Console.WriteLine("Good day, Guys");
+            }  
+            else if (currentHour > 15 && currentHour <= 22)
+            {
+                Console.WriteLine("Good Evening, Guys");
+            }
+                
+            Console.ReadLine();
+        }
+    }
+}
