ea5e64cef1dab35e3bfbc78973da5a14bed5dedd
Mikalai Hil <mikalai.hil@multiparts.net>
2019-07-05 11:18:04 +0300

    Add alphabet

---
+++ Task1/Alphabet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+
+            for(int i = 0x59; i >= 0x41; i--)
+            {
+                string character = i.ToString("X");
+                arr.Add(character);
+            }
+
+            foreach (string elem in arr)
+            {
+                string converted = ((char)Convert.ToInt16(elem, 16)).ToString();
+                Console.WriteLine(converted);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
