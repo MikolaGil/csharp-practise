fc3eaa0470f2aa6427092e0277560cb968ecc039
Mikalai Hil <mikalai.hil@multiparts.net>
2019-07-12 14:49:56 +0300

    Add reverse

---
+++ MainApp/Reverse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+        }
+    }
+}
