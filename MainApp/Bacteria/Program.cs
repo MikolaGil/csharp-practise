eeac6933ad41960b3021deab6e06920a339ec0dd
Mikalai Hil <mikalai.hil@multiparts.net>
2019-07-12 10:31:21 +0300

    Homework 5

---
+++ MainApp/Bacteria/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bacteria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of minutes:");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            double numberOfBacterias = Math.Pow(2, minutes);
+
+            Console.WriteLine("Bacterias amount: " + numberOfBacterias);
+            Console.Read();
+        }
+    }
+}
