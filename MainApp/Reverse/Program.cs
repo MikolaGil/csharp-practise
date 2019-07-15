7bb65ff32373e402ec0e09989d7223d9f722d884
Mikalai Hil <mikalai.hil@multiparts.net>
2019-07-15 09:49:10 +0300

    Array rename

--- MainApp/Reverse/Program.cs
+++ MainApp/Reverse/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-
 namespace Reverse
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
-            int[] reverseArr = new int[arr1.Length];
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            int[] reverseArr = new int[arr.Length];
 
-            for (int i = arr1.Length - 1, j = 0; i >= 0 && i < arr1.Length; i--, j++)
+            for (int i = arr.Length - 1, j = 0; i >= 0 && i < arr.Length; i--, j++)
             {
-                reverseArr[i] = arr1[j];
+                reverseArr[i] = arr[j];
             }
 
-            arr1 = reverseArr;
+            arr = reverseArr;
 
-            foreach (int item in arr1)
+            foreach (int item in arr)
             {
                 Console.WriteLine(item);
             }
