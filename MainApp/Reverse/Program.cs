db6d6485bad11eefe46678c90b2b57fcffe12baa
Mikalai Hil <mikalai.hil@multiparts.net>
2019-07-15 09:41:23 +0300

    Add reverse function

--- MainApp/Reverse/Program.cs
+++ MainApp/Reverse/Program.cs
@@ -7,6 +7,20 @@ namespace Reverse
         static void Main(string[] args)
         {
             int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] reverseArr = new int[arr1.Length];
+
+            for (int i = arr1.Length - 1, j = 0; i >= 0 && i < arr1.Length; i--, j++)
+            {
+                reverseArr[i] = arr1[j];
+            }
+
+            arr1 = reverseArr;
+
+            foreach (int item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
         }
     }
 }
