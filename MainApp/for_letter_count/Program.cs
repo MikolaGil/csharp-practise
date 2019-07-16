afdf38fa0fdb97ad76c273472de456ec2ce9ac96
Mikalai Hil <mikalai.hil@multiparts.net>
2019-07-16 09:16:26 +0300

    Count 'A' letters

---
+++ MainApp/for_letter_count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace for_letter_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input word:");
+            string text = Console.ReadLine().ToUpper();
+            int count = 0;
+
+            foreach (char letter in text)
+            {
+                if (letter == 'A')
+                {
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("There is no 'A' letters in word");
+            }
+            else
+            {
+                Console.WriteLine(count);
+            }
+
+            Console.Read();
+        }
+    }
+}
