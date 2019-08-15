598a718388fa8b2ccee8fdb8ff683be8e3c19bfc
Mikalai Hil <mikalai.hil@multiparts.net>
2019-08-15 17:05:28 +0300

    Added MotorLogger project, implement log4net into project

---
+++ MainApp/MotoLogger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.InitLogger();
+
+            Logger.Log.Info("Test!");
+            Logger.Log.Error("Error!");
+        }
+    }
+}
