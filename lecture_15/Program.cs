9c3e1d2cced8a82c77eb1e5c11ffe6d4beabca6d
Mikola <mikolagil@gmail.com>
2019-08-22 21:00:22 +0300

    Almost works

---
+++ lecture_15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace lecture_15
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Hello World";
+            test.FirstFiveLetters(test);
+        }
+        public static string FirstFiveLetters(this String str, string text)
+        {
+            string result = new string(text.Take(5).ToArray());
+            return result;
+        }
+        
+    }
+    
+}
